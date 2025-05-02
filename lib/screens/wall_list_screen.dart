28fded77a29bb95b2373a54af2f3b7de9bafea33
Jannes Koehler <janneskoehler@gmail.com>
2025-05-03 00:40:06 +0200

    Add wall badge

--- lib/screens/wall_list_screen.dart
+++ lib/screens/wall_list_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 import 'package:pintopia/models/wall.dart';
 import 'package:pintopia/services/firebase_service.dart';
 import 'package:pintopia/services/storage_service.dart';
 import 'package:pintopia/widgets/wall/add_wall_sheet.dart';
 import 'package:pintopia/widgets/wall/edit_wall_sheet.dart';
+import 'package:pintopia/widgets/wall/wall_card.dart';
 
 class WallListScreen extends StatefulWidget {
   const WallListScreen({super.key, required this.storageService});
@@ -91,143 +91,12 @@ class _WallListScreenState extends State<WallListScreen> {
               mainAxisSpacing: 20,
               children: [
                 ...walls.map(
-                  (wall) => Card(
-                    clipBehavior: Clip.antiAlias,
-                    child: InkWell(
-                      onTap: () async {
-                        if (!_isEditMode) {
-                          await widget.storageService
-                              .setWallLastOpened(wall.id);
-                          if (context.mounted) {
-                            context.goNamed(
-                              'wall-detail',
-                              pathParameters: {'id': wall.id},
-                            );
-                          }
-                        }
-                      },
-                      onLongPress: _toggleEditMode,
-                      child: Stack(
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              AspectRatio(
-                                aspectRatio: 16 / 9,
-                                child: wall.assetImageName != null
-                                    ? Image.asset(
-                                        'assets/images/${wall.assetImageName}',
-                                        fit: BoxFit.cover,
-                                      )
-                                    : wall.imageUrl != null
-                                        ? Image.network(
-                                            wall.imageUrl!,
-                                            fit: BoxFit.cover,
-                                            errorBuilder:
-                                                (context, error, stackTrace) {
-                                              return const Center(
-                                                child: Icon(
-                                                  Icons.image_not_supported,
-                                                  size: 50,
-                                                ),
-                                              );
-                                            },
-                                          )
-                                        : Container(
-                                            color: Theme.of(context)
-                                                .colorScheme
-                                                .primaryContainer,
-                                            child: const Center(
-                                              child: Icon(
-                                                Icons.image_not_supported,
-                                                size: 50,
-                                              ),
-                                            ),
-                                          ),
-                              ),
-                              Container(
-                                padding: const EdgeInsets.all(12.0),
-                                child: Center(
-                                  child: Text(
-                                    wall.title,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headlineSmall,
-                                    textAlign: TextAlign.center,
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          if (_isEditMode)
-                            FutureBuilder<bool>(
-                              future:
-                                  widget.storageService.isAdminWall(wall.id),
-                              builder: (context, snapshot) {
-                                if (!snapshot.hasData ||
-                                    snapshot.data != true) {
-                                  return const SizedBox();
-                                }
-
-                                return Positioned(
-                                  top: 8,
-                                  right: 8,
-                                  child: Material(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(20),
-                                    child: InkWell(
-                                      borderRadius: BorderRadius.circular(20),
-                                      onTap: () =>
-                                          _showEditWallSheet(context, wall),
-                                      child: const Padding(
-                                        padding: EdgeInsets.all(8.0),
-                                        child: Icon(Icons.edit),
-                                      ),
-                                    ),
-                                  ),
-                                );
-                              },
-                            ),
-                          FutureBuilder<int>(
-                            future: wall.countNewPinsSinceLastVisit(
-                              widget.storageService,
-                            ),
-                            builder: (context, snapshot) {
-                              if (!snapshot.hasData) {
-                                return const SizedBox();
-                              }
-
-                              if (snapshot.data! > 0) {
-                                return Positioned(
-                                  top: 8,
-                                  right: 8,
-                                  child: Container(
-                                    padding: const EdgeInsets.symmetric(
-                                      horizontal: 8,
-                                      vertical: 4,
-                                    ),
-                                    decoration: BoxDecoration(
-                                      color: Colors.red,
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                    child: Text(
-                                      snapshot.data.toString(),
-                                      style: const TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 12,
-                                      ),
-                                    ),
-                                  ),
-                                );
-                              }
-                              return const SizedBox();
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
+                  (wall) => WallCard(
+                    wall: wall,
+                    storageService: widget.storageService,
+                    isEditMode: _isEditMode,
+                    onLongPress: _toggleEditMode,
+                    onEditWall: _showEditWallSheet,
                   ),
                 ),
 
